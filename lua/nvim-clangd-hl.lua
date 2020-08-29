901ddeac20312f43d76624fbc5895303e8249ab2
Rasmus Michelsen <RMichelsen@protonmail.com>
2020-08-29 14:20:33 +0200

    Fixes the callback to a buffer number

--- lua/nvim-clangd-hl.lua
+++ lua/nvim-clangd-hl.lua
@@ -47,40 +47,62 @@ local token_kind_to_highlight_group = {
     ["InactiveCod"] = "Normal",
 }
 
-function M.handle_highlight_response(_, _, result, _)
-    local line = 0
-    local start = 0
-    local data = result["data"]
-    local data_size = table.getn(data)
+function M.get_highlight_callback(bufnr)
+    return function(_, _, result, _)
+        local win = vim.api.nvim_get_current_win()
+        local column = vim.api.nvim_win_get_cursor(win)[1]
+        local column_count = vim.api.nvim_win_get_height(win)
 
-    local i = 1
-    while (i <= data_size) do
-        local delta_line = data[i]
-        local delta_start = data[i + 1]
-        local length = data[i + 2]
-        local token_kind = token_kinds[data[i + 3] + 1]
+        local line = 0
+        local start = 0
+        local data = result["data"]
+        local data_size = table.getn(data)
 
-        line = line + delta_line
-        if delta_line == 0 then
-            start = start + delta_start
-        else
-            start = delta_start
+        local i = 1
+        while (i <= data_size) do
+            local delta_line = data[i]
+            local delta_start = data[i + 1]
+            local length = data[i + 2]
+            local token_kind = token_kinds[data[i + 3] + 1]
+
+            line = line + delta_line
+            if delta_line == 0 then
+                start = start + delta_start
+            else
+                start = delta_start
+            end
+
+            if line > (column - column_count) and line < (column + column_count) then
+                vim.api.nvim_buf_add_highlight(
+                    bufnr,
+                    -1,
+                    token_kind_to_highlight_group[token_kind],
+                    line,
+                    start,
+                    start + length
+                )
+            end
+
+            i = i + 5
         end
-
-        vim.api.nvim_buf_add_highlight(0, -1, token_kind_to_highlight_group[token_kind], line, start, start + length)
-        i = i + 5
     end
 end
 
-function M.send_highlight_request()
-    local params = { textDocument = { uri = vim.uri_from_bufnr(0) } }
-    vim.lsp.buf_request(0, "textDocument/semanticTokens/full", params, M.handle_highlight_response)
-end
-
 M.on_attach = function(_, _)
-    vim.api.nvim_buf_attach(0, false, {
-        on_lines = M.send_highlight_request
+    local bufnr = vim.api.nvim_get_current_buf()
+    local callback = function()
+        local params = { textDocument = { uri = vim.uri_from_bufnr(bufnr) } }
+        vim.lsp.buf_request(
+            bufnr,
+            "textDocument/semanticTokens/full",
+            params,
+            M.get_highlight_callback(bufnr)
+        )
+    end
+    vim.api.nvim_buf_attach(bufnr, false, {
+        on_lines = callback
     })
+    callback()
 end
 
 return M
